48eeabbd411a629cf840b1a0bd822c2d78a5e0c0
razsa <21493e@gmail.com>
2024-12-22 02:52:17 -0500

    Shopping

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,23 @@
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
+import { ShoppingCart } from "lucide-react-native";
+import { Icon } from "@/components/ui/icon";
 
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { Pressable } from "react-native";
 
 export default function RootLayout() {
-  return <GluestackUIProvider mode="light"><Stack>
+  return <GluestackUIProvider mode="light">
+    <Stack 
+    screenOptions={{ headerRight: () => (
+      <Link href={'/cart'} asChild>
+<Pressable>
+  <Icon as={ShoppingCart}/>
+</Pressable>
+</Link>
+),
+}}
+>
     <Stack.Screen name="index" options={{ title: 'Shop' }} />
     <Stack.Screen name="product/[id]" options={{ title: 'Product' }} />
     </Stack>
