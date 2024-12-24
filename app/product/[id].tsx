6f807722790e9bb00ac73f8992bc45f5b02862f7
razsa <21493e@gmail.com>
2024-12-24 00:28:10 -0500

    Fix getting product by id

--- app/product/[id].tsx
+++ app/product/[id].tsx
@@ -1,6 +1,6 @@
 import { Text } from "@/components/ui/text";
 import { useLocalSearchParams, Stack } from "expo-router";
-import products from "@/assets/products.json";
+import { usePocketbase, Products } from "@/hooks/pocketbase";
 import { Card } from "@/components/ui/card";
 import { Image } from "@/components/ui/image";
 import { VStack } from "@/components/ui/vstack";
@@ -8,19 +8,30 @@ import { Heading } from "@/components/ui/heading";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Box } from "@/components/ui/box";
 import useCart from "@/store/cartStore";
+import { useEffect, useState } from "react";
 
 export default function ProductDetailsScreen() {
-
-  const addProduct = useCart((state) => state.addProduct);
-  const cartItems = useCart((state) => state.items);
-  console.log(JSON.stringify(cartItems, null, 2));
-
+  const [product, setProduct] = useState<Products | null>(null);
+  const addProductToCart = useCart((state) => state.addProduct);
   const { id } = useLocalSearchParams<{ id: string }>();
-  const product = products.find((p) => p.id == Number(id));
+  const { getProductById } = usePocketbase();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      if (id) {
+        const fetchedProduct = await getProductById(id) as Products;
+        setProduct(fetchedProduct);
+      }
+    };
+
+    fetchProduct();
+  }, [id]);
 
   const addToCart = () => {
-    addProduct(product);
-  }
+    if (product) {
+      addProductToCart(product);
+    }
+  };
 
   if (!product) {
     return <Text>Product not found</Text>;
@@ -29,40 +40,36 @@ export default function ProductDetailsScreen() {
   return (
     <Box className="bg-purple-200 flex-1 items-center p-3">
       <Stack.Screen options={{ title: product.name }} />
-    <Card className="p-5 rounded-lg max-w-[960px] w-full flex-1">
-    <Image
-      source={{
-        uri: product.image,
-      }}
-      className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
-      alt={`${product.name} image`}
-      resizeMode="contain"
-    />
-    <Text className="text-sm font-normal mb-2 text-typography-700">
-      {product.name} 
-    </Text>
-    <VStack className="mb-6">
-      <Heading size="md" className="mb-4">
-       {product.price} 
-      </Heading>
-      <Text size="sm">
-        {product.description}
-      </Text>
-    </VStack>
-    <Box className="flex-col sm:flex-row">
-      <Button onPress={addToCart} className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-        <ButtonText size="sm">Add to cart</ButtonText>
-      </Button>
-      <Button
-        variant="outline"
-        className="px-4 py-2 border-outline-300 sm:flex-1"
-      >
-        <ButtonText size="sm" className="text-typography-600">
-          Wishlist
-        </ButtonText>
-      </Button>
+      <Card className="p-5 rounded-lg max-w-[960px] w-full flex-1">
+        <Image
+          source={{
+            uri: product.imageUrl,
+          }}
+          className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
+          alt={`${product.name} image`}
+          resizeMode="contain"
+        />
+        <Text className="text-sm font-normal mb-2 text-typography-700">
+          {product.name}
+        </Text>
+        <VStack className="mb-6">
+          <Heading size="md" className="mb-4">{product.price}</Heading>
+          <Text size="sm">{product.description}</Text>
+        </VStack>
+        <Box className="flex-col sm:flex-row">
+          <Button onPress={addToCart} className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+            <ButtonText size="sm">Add to cart</ButtonText>
+          </Button>
+          <Button
+            variant="outline"
+            className="px-4 py-2 border-outline-300 sm:flex-1"
+          >
+            <ButtonText size="sm" className="text-typography-600">
+              Wishlist
+            </ButtonText>
+          </Button>
+        </Box>
+      </Card>
     </Box>
-  </Card>
-  </Box>
   );
 }
